90bac38087f6b1f88bcbd284faf465abaff20d07
Oliver1334 <oliver.taylor95@outlook.com>
2023-02-21 14:55:51 +0000

    GET /api/reviews implemented with TDD for order and comment_count property, Error handling already in place from the previous Task 3 GET request

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,54 +12,66 @@ afterAll(() => {
     db.end();
 })
 
+
 describe('GET: 200 /api/categories', () => {
     test('responds with an array of category objects with the properties: slug and description', () => {
-        const testResult = [
-            {
-              slug: 'euro game',
-              description: 'Abstact games that involve little luck'
-            },
-            {
-              slug: 'social deduction',
-              description: "Players attempt to uncover each other's hidden role"
-            },
-            {
-              slug: 'dexterity',
-              description: 'Games involving physical skill'
-            },
-            {
-              slug: "children's games",
-              description: 'Games suitable for children'
-            }
-          ];
+    
         return request(app)
         .get('/api/categories')
         .expect(200)
         .then(({body}) => {
             const {categories} = body
-            expect(categories).toEqual(testResult);
+            categories.forEach(category => {
+                expect(category).toHaveProperty('slug', expect.any(String))
+                expect(category).toHaveProperty('description', expect.any(String))
+            })
         })
     })
 
 });
 
-describe('GET: /api/categories errors', () => {
-            test('responds with an error message to a non-existant endpoint', ()=> {
+describe.only('GET: /api/categories or GET: /api/reviews errors', () => {
+            test('responds with an error message to a non-existent endpoint', ()=> {
                return request(app)
                     .get('/api/notAnEndpoint')
                     .expect(404)
                     .then((res)=>{
-                        console.log(res.body)
-                        console.log(res.body.msg);
                         expect(res.body.msg).toBe("Error 404 not found!")
                     })
             })
         });
 
+        describe.only('GET: 200 /api/reviews', () => {
+            test('responds with a reviews array of review objects including an added comment_count property', () => {
+                return request(app)
+                    .get('/api/reviews')
+                    .expect(200)
+                    .then(({body})=>{
+                        const { reviews } = body
+                   reviews.forEach(review => {
+                    expect(review).toHaveProperty('owner', expect.any(String))
+                    expect(review).toHaveProperty('title', expect.any(String))
+                    expect(review).toHaveProperty('review_id', expect.any(Number))
+                    expect(review).toHaveProperty('category', expect.any(String))
+                    expect(review).toHaveProperty('review_img_url', expect.any(String))
+                    expect(review).toHaveProperty('created_at', expect.any(String))
+                    expect(review).toHaveProperty('votes', expect.any(Number))
+                    expect(review).toHaveProperty('designer', expect.any(String))
+                    expect(review).toHaveProperty('comment_count', expect.any(String))
+                    //Don't have many ideas on testing that the comment_count is correct, but the SQL query is working
+                   })
+                        });
+                    })
+            test('Reviews should be sorted by date in ascending order in the array', () => {
+                return request(app)
+                .get('/api/reviews')
+                .expect(200)
+                .then(({body})=>{
+                    const {reviews} = body
+                    expect(reviews).toBeSorted({ ascending: true})
+                })
+            })
+            })
+    
 
-
-
-
-
-
-
+        
---
+++ models/reviews.models.js
@@ -0,0 +1,19 @@
+const db = require("../db/connection");
+
+exports.selectReviews = () => {
+  return db
+    .query(
+      `
+        SELECT reviews.*, COUNT(comments.review_id) AS comment_count
+        FROM reviews
+        LEFT JOIN comments
+        ON comments.review_id = reviews.review_id 
+        GROUP BY reviews.review_id
+        ORDER BY reviews.created_at;
+    `
+    )
+    .then(({ rows }) => rows); 
+};
+
+
+        
