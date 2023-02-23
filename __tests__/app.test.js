f04ba5e5b93258e2bfc7ad69bd7973693ae9011e
Oliver1334 <oliver.taylor95@outlook.com>
2023-02-23 09:35:56 +0000

    Task 5 implemented with TDD and error handling

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,77 +1,110 @@
-const app = require('../app')
-const request = require('supertest')
-const seed = require('../db/seeds/seed')
-const data = require('../db/data/test-data/index')
-const db = require('../db/connection')
+const app = require("../app");
+const request = require("supertest");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data/index");
+const db = require("../db/connection");
 
 beforeEach(() => {
-    return seed(data);
-})
-
-afterAll(() => {
-    db.end();
-})
-
-
-describe('GET: 200 /api/categories', () => {
-    test('responds with an array of category objects with the properties: slug and description', () => {
-    
-        return request(app)
-        .get('/api/categories')
-        .expect(200)
-        .then(({body}) => {
-            const {categories} = body
-            categories.forEach(category => {
-                expect(category).toHaveProperty('slug', expect.any(String))
-                expect(category).toHaveProperty('description', expect.any(String))
-            })
-        })
-    })
-
+  return seed(data);
 });
 
-describe.only('GET: /api/categories or GET: /api/reviews errors', () => {
-            test('responds with an error message to a non-existent endpoint', ()=> {
-               return request(app)
-                    .get('/api/notAnEndpoint')
-                    .expect(404)
-                    .then((res)=>{
-                        expect(res.body.msg).toBe("Error 404 not found!")
-                    })
-            })
+afterAll(() => {
+  db.end();
+});
+
+describe("GET: 200 /api/categories", () => {
+  test("responds with an array of category objects with the properties: slug and description", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then(({ body }) => {
+        const { categories } = body;
+        categories.forEach((category) => {
+          expect(category).toHaveProperty("slug", expect.any(String));
+          expect(category).toHaveProperty("description", expect.any(String));
         });
+      });
+  });
+});
 
-        describe.only('GET: 200 /api/reviews', () => {
-            test('responds with a reviews array of review objects including an added comment_count property', () => {
-                return request(app)
-                    .get('/api/reviews')
-                    .expect(200)
-                    .then(({body})=>{
-                        const { reviews } = body
-                   reviews.forEach(review => {
-                    expect(review).toHaveProperty('owner', expect.any(String))
-                    expect(review).toHaveProperty('title', expect.any(String))
-                    expect(review).toHaveProperty('review_id', expect.any(Number))
-                    expect(review).toHaveProperty('category', expect.any(String))
-                    expect(review).toHaveProperty('review_img_url', expect.any(String))
-                    expect(review).toHaveProperty('created_at', expect.any(String))
-                    expect(review).toHaveProperty('votes', expect.any(Number))
-                    expect(review).toHaveProperty('designer', expect.any(String))
-                    expect(review).toHaveProperty('comment_count', expect.any(String))
-                    //Don't have many ideas on testing that the comment_count is correct, but the SQL query is working
-                   })
-                        });
-                    })
-            test('Reviews should be sorted by date in ascending order in the array', () => {
-                return request(app)
-                .get('/api/reviews')
-                .expect(200)
-                .then(({body})=>{
-                    const {reviews} = body
-                    expect(reviews).toBeSorted({ ascending: true})
-                })
-            })
-            })
-    
+describe("GET: /api/categories or GET: /api/reviews errors", () => {
+  test("responds with an error message to a non-existent endpoint", () => {
+    return request(app)
+      .get("/api/notAnEndpoint")
+      .expect(404)
+      .then((res) => {
+        expect(res.body.msg).toBe("Error 404 not found!");
+      });
+  });
+});
 
-        
+describe("GET: 200 /api/reviews", () => {
+  test("responds with a reviews array of review objects including an added comment_count property", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then(({ body }) => {
+        const { reviews } = body;
+        console.log(reviews);
+        reviews.forEach((review) => {
+          expect(review).toHaveProperty("owner", expect.any(String));
+          expect(review).toHaveProperty("title", expect.any(String));
+          expect(review).toHaveProperty("review_id", expect.any(Number));
+          expect(review).toHaveProperty("category", expect.any(String));
+          expect(review).toHaveProperty("review_img_url", expect.any(String));
+          expect(review).toHaveProperty("created_at", expect.any(String));
+          expect(review).toHaveProperty("votes", expect.any(Number));
+          expect(review).toHaveProperty("designer", expect.any(String));
+          expect(review).toHaveProperty("comment_count", expect.any(String));
+        });
+      });
+  });
+  test("Reviews should be sorted by date in ascending order in the array", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then(({ body }) => {
+        const { reviews } = body;
+        expect(reviews).toBeSortedBy("created_at", { descending: true });
+      });
+  });
+});
+
+describe("GET:200 /api/reviews/:review_id", () => {
+  test("responds with a review object with 9 properties", () => {
+    return request(app)
+      .get("/api/reviews/4")
+      .expect(200)
+      .then(({ body }) => {
+        const { review } = body;
+        expect(review.review_id).toBe(4);
+        expect(review).toHaveProperty("title", expect.any(String));
+        expect(review).toHaveProperty("category", expect.any(String));
+        expect(review).toHaveProperty("designer", expect.any(String));
+        expect(review).toHaveProperty("owner", expect.any(String));
+        expect(review).toHaveProperty("review_body", expect.any(String));
+        expect(review).toHaveProperty("review_img_url", expect.any(String));
+        expect(review).toHaveProperty("created_at", expect.any(String));
+        expect(review).toHaveProperty("votes", expect.any(Number));
+      });
+  });
+});
+
+describe("GET: /api/reviews/:review_id errors", () => {
+  test("responds with a bad request 400 error message for an invalid id", () => {
+    return request(app)
+      .get("/api/reviews/notAnId")
+      .expect(400)
+      .then((res) => {
+        expect(res.body.msg).toBe("Bad Request!");
+      });
+  });
+  test("404: responds with custom error msg when sent a query with a valid ID but non-existent review_id", () => {
+    return request(app)
+      .get("/api/reviews/999")
+      .expect(404)
+      .then((res) => {
+        expect(res.text).toBe("Review_id not found");
+      });
+  });
+});
