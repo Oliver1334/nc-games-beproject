a497f5d21cd5ba42e8f490d8f5390441eca22737
Oliver1334 <oliver.taylor95@outlook.com>
2023-02-24 11:24:13 +0000

    Task 7 implemented with full TDD and error handling

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -109,56 +109,152 @@ describe("GET: /api/reviews/:review_id errors", () => {
 });
 
 describe("GET:200 /api/reviews/:review_id/comments", () => {
-    test("responds with an array of comments objects for the given review_id with 6 properties", () => {
-      return request(app)
-        .get("/api/reviews/3/comments")
-        .expect(200)
-        .then(({ body }) => {
-          const {comments} = body
-          comments.forEach((comment) => {
+  test("responds with an array of comments objects for the given review_id with 6 properties", () => {
+    return request(app)
+      .get("/api/reviews/3/comments")
+      .expect(200)
+      .then(({ body }) => {
+        const { comments } = body;
+        comments.forEach((comment) => {
           expect(comment.review_id).toBe(3);
           expect(comment).toHaveProperty("comment_id", expect.any(Number));
           expect(comment).toHaveProperty("body", expect.any(String));
           expect(comment).toHaveProperty("author", expect.any(String));
           expect(comment).toHaveProperty("votes", expect.any(Number));
           expect(comment).toHaveProperty("created_at", expect.any(String));
-        })
         });
-    });
-    test("The returned comments array should be ordered with the most recent comments first!", () => {
-      return request(app)
+      });
+  });
+  test("The returned comments array should be ordered with the most recent comments first!", () => {
+    return request(app)
       .get("/api/reviews/3/comments")
       .expect(200)
       .then(({ body }) => {
-        const {comments} = body;
+        const { comments } = body;
         expect(comments).toBeSortedBy("created_at", { descending: true });
       });
-    })
-    test("When a Valid review ID is used but there are no comments for that ID a blank comments array is returned", () => {
-      return request(app)
+  });
+  test("When a Valid review ID is used but there are no comments for that ID a blank comments array is returned", () => {
+    return request(app)
       .get("/api/reviews/5/comments")
       .expect(200)
       .then(({ body }) => {
-        const {comments} = body;
-        expect(comments).toEqual([])
+        const { comments } = body;
+        expect(comments).toEqual([]);
       });
-    })
+  });
+});
+
+describe("GET: /api/reviews/:review_id/comments errors", () => {
+  test("Responds with a 404 custom error message when a valid but non existent review ID is input", () => {
+    return request(app)
+      .get("/api/reviews/15/comments")
+      .expect(404)
+      .then((res) => {
+        expect(res.text).toBe("Review_id not found");
+      });
+  });
+});
+
+describe("POST: 201 /api/reviews/:review_id/comments", () => {
+  test("Adds a comment with a request body object of username and body for a review to the comments data and returns the comment", () => {
+    const reqBody = {
+      username: "dav3rid",
+      body: "I hate werewolves, I would rather play something about vampires!",
+    };
+    return request(app)
+      .post("/api/reviews/3/comments")
+      .send(reqBody)
+      .expect(201)
+      .then(({ body }) => {
+        const { comment } = body;
+        expect(comment[0].comment_id).toBe(7);
+        expect(comment[0].body).toBe(
+          "I hate werewolves, I would rather play something about vampires!"
+        );
+        expect(comment[0].review_id).toBe(3);
+        expect(comment[0].author).toBe("dav3rid");
+        expect(comment[0].votes).toBe(0);
+        expect(comment[0]).toHaveProperty("created_at", expect.any(String));
+      });
+  });
+  test("Ignores extra data from a request body and makes a successful post", () => {
+    const reqBody = {
+      username: "mallionaire",
+      body: "Boy do i LOVE werewolves!",
+      extraData: "Really really love werewolves.",
+    };
+    return request(app)
+      .post("/api/reviews/3/comments")
+      .send(reqBody)
+      .expect(201)
+      .then(({ body }) => {
+        const { comment } = body;
+        expect(comment[0].comment_id).toBe(7);
+        expect(comment[0].body).toBe("Boy do i LOVE werewolves!");
+        expect(comment[0].review_id).toBe(3);
+        expect(comment[0].author).toBe("mallionaire");
+        expect(comment[0].votes).toBe(0);
+        expect(comment[0]).toHaveProperty("created_at", expect.any(String));
+      });
+  });
+});
+
+describe("POST:/api/reviews/:review_id/comments errors", () => {
+  test("responds with a 404 not found for a request body with a username that doesn't exist in the database", () => {
+    const reqBody = {
+      username: "unknown666",
+      body: "I hate werewolves, I would rather play something about vampires!",
+    };
+    return request(app)
+      .post("/api/reviews/3/comments")
+      .send(reqBody)
+      .expect(404)
+      .then((res) => {
+        expect(res.text).toBe("Username not found!");
+      });
+  });
+  test("responds with a 400 error when request body has incorrect values", () => {
+    const reqBody = {
+      author: "dav3rid",
+      text: "I hate werewolves, I would rather play something about vampires!",
+    };
+    return request(app)
+      .post("/api/reviews/3/comments")
+      .expect(400)
+      .then((res) => {
+        expect(res.body.msg).toBe("Bad request, incorrect values!");
+      });
   });
 
-  describe("GET: /api/reviews/:review_id/comments errors", () => {
-    test("Responds with a 404 custom error message when a valid but non existent review ID is input", () => {
-      return request(app)
-        .get("/api/reviews/15/comments")
-        .expect(404)
-        .then((res) => {
-          expect(res.text).toBe("Review_id not found");
-        });
-    });
-  })
+  test("responds with a 400 error when given an invalid review ID", () => {
+    const reqBody = {
+      username: "dav3rid",
+      body: "I hate werewolves, I would rather play something about vampires!",
+    };
+    return request(app)
+      .post("/api/reviews/notAnId/comments")
+      .send(reqBody)
+      .expect(400)
+      .then((res) => {
+        expect(res.body.msg).toBe("Bad Request!");
+      });
+  });
+  test("404: responds with custom error msg when sent a query with a review_id that is out of range", () => {
+    const reqBody = {
+      username: "dav3rid",
+      body: "I hate werewolves, I would rather play something about vampires!",
+    };
+    return request(app)
+      .post("/api/reviews/999/comments")
+      .send(reqBody)
+      .expect(404)
+      .then((res) => {
+        expect(res.text).toBe("Review_id not found");
+      });
+  });
+});
 
-
-
-
-
-
-
+// error post from user that doesn't exist
+// error review id out of range
+// error review not a number
--- models/comments.models.js
+++ models/comments.models.js
@@ -1,31 +1,74 @@
-const db = require("../db/connection")
+const db = require("../db/connection");
 
 exports.selectComments = (review_id) => {
-    return db
+  return db
     .query(
-        `
+      `
         SELECT *
         FROM reviews
         WHERE review_id = $1;
-        `, [review_id]
+        `,
+      [review_id]
     )
     .then(({ rows }) => {
-    if (rows.length === 0){
-        return Promise.reject({status: 404, message: "Review_id not found"})
-    } else {
-        return db
-        .query(
-            `
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, message: "Review_id not found" });
+      } else {
+        return db.query(
+          `
             SELECT *
             FROM comments
             WHERE review_id = $1
             ORDER BY created_at DESC
-            `, [review_id]
-        )
-        .then(({rows}) =>{
-            return rows
-        })
-    }
-        
-    } 
-)};
+            `,
+          [review_id]
+        );
+      }
+    })
+    .then(({ rows }) => {
+      return rows;
+    });
+};
+
+exports.insertComment = (review_id, username, body) => {
+  return db
+    .query(
+      `
+      SELECT *
+      FROM users
+      WHERE username = $1;
+    `,
+      [username]
+    )
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, message: "Username not found!" });
+      } else {
+        return db.query(
+          `
+          SELECT *
+          FROM reviews
+          WHERE review_id = $1
+          `,
+          [review_id]
+        );
+      }
+    })
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, message: "Review_id not found" });
+      } else {
+        return db.query(
+          `
+        INSERT INTO comments (body, author, review_id)
+        VALUES ($1,$2,$3)
+        RETURNING *;
+      `,
+          [body, username, review_id]
+        );
+      }
+    })
+    .then(({ rows }) => {
+      return rows;
+    });
+};
